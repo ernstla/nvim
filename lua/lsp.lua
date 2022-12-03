28815eb460990a5664744b85de1f4246943fa165
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-12-03 14:47:41 +0100

    Change deprecated call of update_capabilities

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -51,7 +51,8 @@ end
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
 
 -- Enable the following language servers
 local servers = { 'clangd', 'tsserver', 'vuels', 'svelte', 'jsonls' }
