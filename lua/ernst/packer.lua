ca5c247dc52448a3e428ac471a53373400a7bf32
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-12-22 20:04:44 +0100

    Add and configure lir.nvim (filebeagle alternative)

--- lua/ernst/packer.lua
+++ lua/ernst/packer.lua
@@ -2,9 +2,16 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
+
     use {
 	'nvim-telescope/telescope.nvim', 
 	tag = '0.1.0', 
+	requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    -- filebeagle and dirvish alternative
+    use {
+        'tamago324/lir.nvim',
 	requires = { {'nvim-lua/plenary.nvim'} }
     }
 end)
