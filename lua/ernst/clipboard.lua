10b974ec51d27f68c4b65c7d831aa972b8427ef6
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-12-22 20:54:35 +0100

    Update clipboard settings

--- lua/ernst/clipboard.lua
+++ lua/ernst/clipboard.lua
@@ -1,12 +1,24 @@
-vim.g.clipboard = {
-    name = "win32yank",
-    copy = {
-        ["+"] = "win32yank.exe -i",
-        ["*"] = "win32yank.exe -i",
-    },
-    paste = {
-        ["+"] = "win32yank.exe -o",
-        ["*"] = "win32yank.exe -o",
-    },
-    cache_enabled = false,
-}
+if vim.fn.has('wsl') == true then
+    vim.g.clipboard = {
+        name = "win32yank",
+        copy = {
+            ["+"] = "win32yank.exe -i",
+            ["*"] = "win32yank.exe -i",
+        },
+        paste = {
+            ["+"] = "win32yank.exe -o",
+            ["*"] = "win32yank.exe -o",
+        },
+        cache_enabled = true,
+    }
+
+    vim.api.nvim_exec(
+        [[
+            augroup WSLYank
+                autocmd!
+                autocmd TextYankPost * :call system('clip.exe ',@")
+            augroup end
+        ]],
+        false
+    )
+end
