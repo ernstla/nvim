0622da32ee085d7079d32a32587e5685a602cab2
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-12-27 17:30:03 +0100

    Add lsp-zero package and configuration

--- lua/ernst/packer.lua
+++ lua/ernst/packer.lua
@@ -13,13 +13,13 @@ return require('packer').startup(function(use)
 
     use {
         'ThePrimeagen/harpoon',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
     use {
-        'nvim-telescope/telescope.nvim', 
-        tag = '0.1.0', 
-        requires = { {'nvim-lua/plenary.nvim'} }
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.0',
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
     use 'nvim-tree/nvim-web-devicons'
@@ -27,13 +27,36 @@ return require('packer').startup(function(use)
     -- filebeagle and dirvish alternative
     use {
         'tamago324/lir.nvim',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
 
     -- color scheme
-    use {'rose-pine/neovim', as = 'rose-pine'}
+    use { 'rose-pine/neovim', as = 'rose-pine' }
 
-    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     use 'nvim-treesitter/playground'
+
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
 end)
