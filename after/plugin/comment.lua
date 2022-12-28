76cd08e94cde0c0405a36f0351854b51dd0aab01
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-12-28 16:40:22 +0100

    Configure commenter mappings

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -1,45 +1,51 @@
 require('Comment').setup({
-    ---Add a space b/w comment and the line
-    padding = true,
-    ---Whether the cursor should stay at its position
-    sticky = true,
-    ---Lines to be ignored while (un)comment
-    ignore = nil,
-    ---LHS of toggle mappings in NORMAL mode
+    padding = true, -- Add a space b/w comment and the line
+    sticky = true, -- Whether the cursor should stay at its position
+    ignore = nil, -- Lines to be ignored while (un)comment
+    pre_hook = nil, -- Function to call before (un)comment
+    post_hook = nil, -- Function to call after (un)comment
+
+    -- LHS of toggle mappings in NORMAL mode
     toggler = {
-        ---Line-comment toggle keymap
-        -- line = 'gcc',
-        line = '<leader>ci',
-        ---Block-comment toggle keymap
-        block = 'gbc',
+        line = 'gcc', -- Line-comment toggle keymap
+        -- line = '<leader>ci',
+        block = 'gbc', -- Block-comment toggle keymap
     },
-    ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+
+    -- LHS of operator-pending mappings in NORMAL and VISUAL mode
     opleader = {
-        ---Line-comment keymap
-        --line = 'gc',
-        line = '<leader>ci',
-        ---Block-comment keymap
-        block = 'gb',
+        -- Line-comment keymap
+        line = '<leader>ci', --line = 'gc',
+        block = 'gb', -- Block-comment keymap
     },
-    ---LHS of extra mappings
+
+    -- LHS of extra mappings
     extra = {
-        ---Add comment on the line above
-        above = 'gcO',
-        ---Add comment on the line below
-        below = 'gco',
-        ---Add comment at the end of line
-        eol = 'gcA',
+        above = 'gcO', -- Add comment on the line above
+        below = 'gco', -- Add comment on the line below
+        eol = 'gcA', -- Add comment at the end of line
     },
-    ---Enable keybindings
-    ---NOTE: If given `false` then the plugin won't create any mappings
+
+    -- Enable keybindings
+    -- NOTE: If given `false` then the plugin won't create any mappings
     mappings = {
-        ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+        -- Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
         basic = true,
-        ---Extra mapping; `gco`, `gcO`, `gcA`
-        extra = true,
+        extra = true, -- Extra mapping; `gco`, `gcO`, `gcA`
     },
-    ---Function to call before (un)comment
-    pre_hook = nil,
-    ---Function to call after (un)comment
-    post_hook = nil,
 })
+
+local api = require('Comment.api')
+local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
+
+vim.keymap.set(
+    'n',
+    '<c-_>', -- <C-_> means <C-/> -- see :help :map-special-keys
+    "<Cmd>lua require('Comment.api').toggle.linewise.current(vim.fn.visualmode())<CR>",
+    { noremap = true, silent = true, nowait = true }
+)
+
+vim.keymap.set('x', '<c-_>', function()
+    vim.api.nvim_feedkeys(esc, 'nx', false)
+    api.toggle.linewise(vim.fn.visualmode())
+end)
