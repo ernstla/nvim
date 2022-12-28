15e3abc0bc5f324503b0474266e2966580515b12
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-12-28 09:31:33 +0100

    Add lualine plugin

--- lua/ernst/packer.lua
+++ lua/ernst/packer.lua
@@ -37,6 +37,11 @@ return require('packer').startup(function(use)
         }
     }
 
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+
 
     -- color scheme
     use { 'rose-pine/neovim', as = 'rose-pine' }
