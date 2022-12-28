793dda2a8ddaefa84c20f92693dc8be7cad6e0ce
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-12-28 09:24:20 +0100

    Show lir in float, center it in the current window

--- after/plugin/lir.lua
+++ after/plugin/lir.lua
@@ -1,78 +1,109 @@
-local actions = require'lir.actions'
+local actions = require 'lir.actions'
 local mark_actions = require 'lir.mark.actions'
-local clipboard_actions = require'lir.clipboard.actions'
+local clipboard_actions = require 'lir.clipboard.actions'
+local float = require 'lir.float'
 
-require'lir'.setup {
-  show_hidden_files = false,
-  ignore = {}, -- { ".DS_Store" "node_modules" } etc.
-  devicons_enable = true,
-  mappings = {
-    ['<CR>']     = actions.edit,
-    ['<C-s>'] = actions.split,
-    ['<C-v>'] = actions.vsplit,
-    ['<C-t>'] = actions.tabedit,
 
-    ['-']     = actions.up,
-    ['q']     = actions.quit,
+-- Construct the Lir floating window options according to the window we are
+-- currently in. The position of the window will be centered in the current
+-- window, thus not blocking other windows if opened.
+---@return table<string, any>
+local function construct_win_opts()
+    local winpos = vim.api.nvim_win_get_position(0)
+    local winwidth = vim.api.nvim_win_get_width(0)
+    local winheight = vim.api.nvim_win_get_height(0)
 
-    ['K']     = actions.mkdir,
-    ['+']     = actions.newfile,
-    ['R']     = actions.rename,
-    ['@']     = actions.cd,
-    ['Y']     = actions.yank_path,
-    ['.']     = actions.toggle_show_hidden,
-    ['D']     = actions.delete,
+    local width = math.min(80, winwidth - 20)
+    local height = winheight - 50
+    local row = (winheight / 3) - (height / 3) - 1
 
-    ['J'] = function()
-        mark_actions.toggle_mark()
-        vim.cmd('normal! j')
-    end,
-    ['C'] = clipboard_actions.copy,
-    ['X'] = clipboard_actions.cut,
-    ['P'] = clipboard_actions.paste,
-  },
-  float = {
-    winblend = 0,
-    curdir_window = {
-      enable = false,
-      highlight_dirname = false
+    if winheight <= 50 then
+        height = winheight - 20
+        row = (winheight / 2) - (height / 2) - 1
+    end
+    local col = (winwidth / 2) - (width / 2)
+
+    return {
+        width = width,
+        height = height,
+        row = row + winpos[1],
+        col = col + winpos[2],
+    }
+end
+
+require 'lir'.setup {
+    show_hidden_files = false,
+    ignore = { ".DS_Store" },
+    devicons_enable = true,
+    mappings = {
+        ['<CR>']  = actions.edit,
+        ['<C-s>'] = actions.split,
+        ['<C-v>'] = actions.vsplit,
+        ['<C-t>'] = actions.tabedit,
+
+        ['-'] = actions.up,
+        ['q'] = actions.quit,
+        ['<ESC>'] = actions.quit,
+        ['<C-c>'] = actions.quit,
+
+        ['K'] = actions.mkdir,
+        ['+'] = actions.newfile,
+        ['R'] = actions.rename,
+        ['@'] = actions.cd,
+        ['Y'] = actions.yank_path,
+        ['.'] = actions.toggle_show_hidden,
+        ['D'] = actions.delete,
+
+        ['J'] = function()
+            mark_actions.toggle_mark()
+            vim.cmd('normal! j')
+        end,
+        ['C'] = clipboard_actions.copy,
+        ['X'] = clipboard_actions.cut,
+        ['P'] = clipboard_actions.paste,
     },
+    float = {
+        winblend = 0,
+        win_opts = construct_win_opts,
+        curdir_window = {
+            enable = true,
+            highlight_dirname = false
+        },
 
-    -- -- You can define a function that returns a table to be passed as the third
-    -- -- argument of nvim_open_win().
-    -- win_opts = function()
-    --   local width = math.floor(vim.o.columns * 0.8)
-    --   local height = math.floor(vim.o.lines * 0.8)
-    --   return {
-    --     border = {
-    --       "+", "─", "+", "│", "+", "─", "+", "│",
-    --     },
-    --     width = width,
-    --     height = height,
-    --     row = 1,
-    --     col = math.floor((vim.o.columns - width) / 2),
-    --   }
-    -- end,
-  },
-  hide_cursor = true,
-  on_init = function()
-    -- use visual mode
-    vim.api.nvim_buf_set_keymap(
-      0,
-      "x",
-      "J",
-      ':<C-u>lua require"lir.mark.actions".toggle_mark("v")<CR>',
-      { noremap = true, silent = true }
-    )
+        -- -- You can define a function that returns a table to be passed as the third
+        -- -- argument of nvim_open_win().
+        -- win_opts = function()
+        --   local width = math.floor(vim.o.columns * 0.8)
+        --   local height = math.floor(vim.o.lines * 0.8)
+        --   return {
+        --     border = {
+        --       "+", "─", "+", "│", "+", "─", "+", "│",
+        --     },
+        --     width = width,
+        --     height = height,
+        --     row = 1,
+        --     col = math.floor((vim.o.columns - width) / 2),
+        --   }
+        -- end,
+    },
+    hide_cursor = true,
+    on_init = function()
+        -- use visual mode
+        vim.api.nvim_buf_set_keymap(
+            0,
+            "x",
+            "J",
+            ':<C-u>lua require"lir.mark.actions".toggle_mark("v")<CR>',
+            { noremap = true, silent = true }
+        )
 
-    -- echo cwd
-    vim.api.nvim_echo({ { vim.fn.expand("%:p"), "Normal" } }, false, {})
-  end,
+        -- echo cwd
+        vim.api.nvim_echo({ { vim.fn.expand("%:p"), "Normal" } }, false, {})
+    end,
 }
 
-vim.api.nvim_set_keymap('n', '-', [[<Cmd> execute 'silent! edit ' .. expand('%:p:h')<CR>]], { noremap = true })
---vim.g.loaded_netrw = 1
---vim.g.loaded_netrwPlugin = 1
+-- vim.api.nvim_set_keymap('n', '-', [[<Cmd> execute 'silent! edit ' .. expand('%:p:h')<CR>]], { noremap = true })
+vim.keymap.set('n', '-', function() float.init() end, { noremap = true })
 
 -- custom folder icon
 --require'nvim-web-devicons'.set_icon({
@@ -82,4 +113,3 @@ vim.api.nvim_set_keymap('n', '-', [[<Cmd> execute 'silent! edit ' .. expand('%:p
 --    name = "LirFolderNode"
 --  }
 --})
-
