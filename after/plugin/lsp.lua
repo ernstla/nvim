b59a42ecc5fe967249ce14ab7affe83187f1a9a3
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-12-30 17:15:34 +0100

    Expand to first item in cmp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -120,27 +120,28 @@ cmp.setup {
     mapping = cmp.mapping.preset.insert {
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-Space>'] = cmp.mapping.complete({ behavior = cmp.SelectBehavior.Insert }),
         ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         },
         ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
+            if luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
+            elseif cmp.visible() then
+                cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
             elseif has_words_before() then
                 cmp.complete()
+                cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
             else
                 fallback()
             end
         end, { 'i', 's' }),
         ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
+            if luasnip.jumpable(-1) then
                 luasnip.jump(-1)
+            elseif cmp.visible() then
+                cmp.select_prev_item()
             else
                 fallback()
             end
