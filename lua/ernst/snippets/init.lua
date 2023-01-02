b7b1a2e8d115d9762fec97bf7c189028af0232f6
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-01-02 16:49:34 +0100

    Add PHP snippets

--- lua/ernst/snippets/init.lua
+++ lua/ernst/snippets/init.lua
@@ -3,4 +3,5 @@ local ls = require("luasnip")
 ls.add_snippets(nil, {
     all = require('ernst/snippets/all'),
     svelte = require('ernst/snippets/svelte'),
+    php = require('ernst/snippets/php'),
 })
---
+++ lua/ernst/snippets/php.lua
@@ -0,0 +1,74 @@
+local ls = require("luasnip")
+
+local snippet = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+return {
+    snippet('strict', {
+        t({ '<?php', '', 'declare(strict_types=1);', '', '' }),
+        i(0)
+    }),
+    snippet('php', {
+        t('<?php '),
+        i(0),
+        t(' ?>'),
+    }),
+    snippet('=', {
+        t('<?= '),
+        i(0),
+        t(' ?>'),
+    }),
+    snippet('_', {
+        t("<?= _('"),
+        i(0),
+        t("') ?>"),
+    }),
+    snippet('construct', {
+        t('public function __construct('),
+        i(1),
+        t({ ') {', '\t' }),
+        i(0),
+        t({ '', '}' }),
+    }),
+    snippet('log', {
+        t('error_log('),
+        i(0),
+        t(');'),
+    }),
+    snippet('logs', {
+        t("error_log('"),
+        i(0),
+        t("');"),
+    }),
+    snippet('logr', {
+        t('error_log(print_r('),
+        i(0),
+        t(', true));'),
+    }),
+    snippet('print', {
+        t('print('),
+        i(0),
+        t(', "\\n");'),
+    }),
+    snippet('ps', {
+        t("print('"),
+        i(0),
+        t("'" .. '. "\\n");'),
+    }),
+    snippet('prints', {
+        t("print('"),
+        i(0),
+        t("'" .. '. "\\n");'),
+    }),
+    snippet('pr', {
+        t('print_r('),
+        i(0),
+        t(');'),
+    }),
+    snippet('printr', {
+        t('print_r('),
+        i(0),
+        t(');'),
+    }),
+}
