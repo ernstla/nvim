6e886a916f40fa9d451c80f5f5e6d8b0b5a857df
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-01-24 19:39:48 +0100

    Kill snippet session when leaving insert mode

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -77,6 +77,25 @@ map('s', '<c-j>', "<cmd>lua require('luasnip').jump(1)<cr>", opts)
 map('i', '<c-k>', "<cmd>lua require('luasnip').jump(-1)<cr>", opts)
 map('s', '<c-k>', "<cmd>lua require('luasnip').jump(-1)<cr>", opts)
 
+-- Kill snippets session when leaving insert mode
+local unlinkgrp = vim.api.nvim_create_augroup(
+    'UnlinkSnippetOnModeChange',
+    { clear = true }
+)
+vim.api.nvim_create_autocmd('ModeChanged', {
+    group = unlinkgrp,
+    pattern = { 's:n', 'i:*' },
+    desc = 'Forget the current snippet when leaving the insert mode',
+    callback = function(evt)
+        if luasnip.session
+            and luasnip.session.current_nodes[evt.buf]
+            and not luasnip.session.jump_active
+        then
+            luasnip.unlink_current()
+        end
+    end,
+})
+
 require('ernst/snippets')
 
 local kind_icons = {
