dbcc554941339fe258b759cc074835192e4e5d54
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-07-27 08:48:03 +0200

    Add svelte files to null-ls config

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -2,7 +2,9 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettier.with({
+            extra_filetypes = { "svelte" },
+        }),
         null_ls.builtins.formatting.phpcsfixer,
         --null_ls.builtins.diagnostics.php,
         --null_ls.builtins.diagnostics.phpcs,
