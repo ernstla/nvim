40baaf7a0d4459ca6020d5dc64ec579c712c889a
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-10-02 15:56:41 +0200

    Add macros

---
+++ lua/ernst/macros.lua
@@ -0,0 +1,43 @@
+local setreg = vim.fn.setreg
+local replace = vim.api.nvim_replace_termcodes
+
+function replace(s)
+    return vim.api.nvim_replace_termcodes(s, true, true, true)
+end
+
+-- Pest to PHPUnit - transform  test() to a public function
+setreg(
+    'T',
+    replace(
+        '/test(<CR>vllllllhcpublic function<CR><ESC>/[\'"]<CR>' ..
+        'D0:s/\\<./\\u&/g<CR>:s/ //g<CR>' ..
+        'itest<ESC>A(): void<ESC>kJo{<ESC>/^\\s*});<CR>cc}<ESC>:w<CR>'
+    )
+)
+
+-- Pest to PHPUnit - move test()->throws into block
+setreg(
+    'X',
+    replace(
+        ':let oldpattern=@/<CR>k/->throws<CR>dt;?test(<CR>' ..
+        'o<ESC>pa;<ESC>I$this<ESC>o<ESC>?test(<CR>:let @/=oldpattern<CR>k'
+    )
+)
+
+-- Pest to PHPUnit - transform expect()->toBe
+setreg(
+    'E',
+    replace(
+        ':let oldpattern=@/<CR>^cw$this->assertEquals<ESC>la, <ESC>' ..
+        '$xx^/)->toBe<CR>vllllllldD^/,<CR>P$a);<ESC>:let @/=oldpattern<CR>j^'
+    )
+)
+
+-- Pest to PHPUnit - transform expect()->toBeInstanceOf
+setreg(
+    'I',
+    replace(
+        ':let oldpattern=@/<CR>^cw$this->assertInstanceOf<ESC>la, <ESC>/)' ..
+        '->toBeInstanceOf(<CR>vlllllllllllllllllddt)?this->assert<CR>:let @/=oldpattern<CR>f,Pj^'
+    )
+)
