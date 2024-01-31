3e46d0bf3d08272663dcf32dd9919366fe1887b3
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-01-31 16:13:50 +0100

    Tag latest telescope version 0.1.5

--- lua/ernst/lazy.lua
+++ lua/ernst/lazy.lua
@@ -31,7 +31,7 @@ require("lazy").setup({
 
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.0',
+        tag = '0.1.5',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
 
