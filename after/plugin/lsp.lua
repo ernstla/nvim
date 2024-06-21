9e44c3b597d90c3f479115be7f91fa4f9607c427
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-06-21 10:44:25 +0200

    Fix cmp/luasnip behaviour

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -219,7 +219,7 @@ cmp.setup {
             select = true,
         },
         ['<tab>'] = cmp.mapping(function(fallback)
-            if luasnip.expand_or_jumpable() then
+            if not cmp.visible() and luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
             elseif cmp.visible() then
                 cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
