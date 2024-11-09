fa08de895125bc9f2df7c4bf9f57a4b0055abbe7
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-11-09 15:16:56 +0100

    Add gitsings jump keybindings

--- lua/ernst/plugins/gitsigns.lua
+++ lua/ernst/plugins/gitsigns.lua
@@ -9,27 +9,27 @@ return { {
             changedelete = { text = '◦' },
             untracked = { xt = '┆' },
         },
-        signcolumn = true,     -- Toggle with `:Gitsigns toggle_signs`
-        numhl = false,         -- Toggle with `:Gitsigns toggle_numhl`
-        linehl = false,        -- Toggle with `:Gitsigns toggle_linehl`
-        word_diff = false,     -- Toggle with `:Gitsigns toggle_word_diff`
+        signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+        numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
+        linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
+        word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
         watch_gitdir = {
             interval = 1000,
             follow_files = true
         },
         attach_to_untracked = true,
-        current_line_blame = false,     -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
         current_line_blame_opts = {
             virt_text = true,
-            virt_text_pos = 'eol',     -- 'eol' | 'overlay' | 'right_align'
+            virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
             delay = 1000,
             ignore_whitespace = false,
         },
         current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
         sign_priority = 6,
         update_debounce = 100,
-        status_formatter = nil,      -- Use default
-        max_file_length = 40000,     -- Disable if file is longer than this (in lines)
+        status_formatter = nil,  -- Use default
+        max_file_length = 40000, -- Disable if file is longer than this (in lines)
         preview_config = {
             -- Options passed to nvim_open_win
             border = 'single',
@@ -38,5 +38,26 @@ return { {
             row = 0,
             col = 1
         }
-    }
+    },
+    config = function(spec)
+        require('gitsigns').setup(spec.opts)
+
+        local gs = package.loaded.gitsigns
+
+        vim.keymap.set('n', '<leader>]', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function()
+                gs.next_hunk()
+            end)
+            return '<Ignore>'
+        end, { noremap = true, silent = true, nowait = true, desc = "next change hunk", expr = true })
+
+        vim.keymap.set('n', '<leader>[', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function()
+                gs.prev_hunk()
+            end)
+            return '<Ignore>'
+        end, { noremap = true, silent = true, nowait = true, desc = "previous change hunk", expr = true })
+    end
 } }
