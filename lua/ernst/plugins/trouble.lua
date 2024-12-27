b775ba6d9e0dded48d76fc079796f480533d5c8f
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-12-27 18:57:47 +0100

    Update trouble configuration

--- lua/ernst/plugins/trouble.lua
+++ lua/ernst/plugins/trouble.lua
@@ -2,12 +2,28 @@ return { {
     'folke/trouble.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
-        mode = "document_diagnostics"
+        mode = "workspace_diagnostics",
+        focus = true, -- Focus the window when opened
+        win = { size = 20 },
+        modes = {
+            symbols = {
+                mode = "lsp_document_symbols",
+                focus = true,
+                win = { position = "right", size = 50 },
+            },
+        },
     },
     config = function(spec)
         require('trouble').setup(spec.opts)
-        vim.keymap.set("n", "<leader>f", "<cmd>TroubleToggle<cr>",
-            { silent = true, noremap = true }
+
+        require("which-key").add(
+            {
+                {
+                    { "<leader>te", "<cmd>Trouble diagnostics toggle<cr>",              desc = "Trouble: toogle errors",  nowait = true, remap = false },
+                    { "<leader>tE", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Trouble: toogle errors",  nowait = true, remap = false },
+                    { "<leader>ts", "<cmd>Trouble symbols toggle<cr>",                  desc = "Trouble: toogle symbols", nowait = true, remap = false },
+                }
+            }
         )
     end
 } }
