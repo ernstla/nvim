56945b21e6795a3dc50f8c4e5b0097be6a40a1d6
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-12-30 12:45:39 +0100

    Deactivate smear cursor

--- lua/ernst/plugins/smear-cursor.lua
+++ lua/ernst/plugins/smear-cursor.lua
@@ -1,10 +1,12 @@
-return {
-    "sphamba/smear-cursor.nvim",
-    opts = {
-        cursor_color = "#ff8800",
-        stiffness = 0.8,               -- 0.6      [0, 1]
-        trailing_stiffness = 0.5,      -- 0.3      [0, 1]
-        distance_stop_animating = 0.1, -- 0.1      > 0
-        hide_target_hack = true,       -- true     boolean
-    },
-}
+return {}
+-- return {
+--     "sphamba/smear-cursor.nvim",
+--     opts = {
+--         cursor_color = "#ff8800",
+--         stiffness = 0.8,               -- 0.6      [0, 1]
+--         trailing_stiffness = 0.5,      -- 0.3      [0, 1]
+--         distance_stop_animating = 0.1, -- 0.1      > 0
+--         hide_target_hack = true,       -- true     boolean
+--         smear_to_cmd = false
+--     },
+-- }
