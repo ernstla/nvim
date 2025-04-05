65ff819302fe94b4fbb99453aa452c8c7de0e026
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2025-04-05 21:33:01 +0200

    Move diagnostice config to lsp module

--- lua/ernst/lsp.lua
+++ lua/ernst/lsp.lua
@@ -1,3 +1,17 @@
+vim.diagnostic.config {
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = ' ',
+            [vim.diagnostic.severity.WARN] = ' ',
+            [vim.diagnostic.severity.INFO] = ' ',
+            [vim.diagnostic.severity.HINT] = '',
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = 'DiagnosticSignErrorNumber',
+        },
+    },
+}
+
 vim.lsp.enable({
     'eslint',
     'go',
