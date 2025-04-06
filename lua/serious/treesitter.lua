85d167840674de96eb9eb7465ccc2f91c8d5b896
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2025-04-06 17:37:22 +0200

    Adjust colors

--- lua/serious/treesitter.lua
+++ lua/serious/treesitter.lua
@@ -1,41 +1,52 @@
 local c = require('serious/colors')
 
 return {
-    ['@keyword'] = { fg = c.blue7 },
     ['@conditional'] = { fg = c.blue4 },
+    ['@keyword'] = { fg = c.blue7 },
     ['@tag'] = { fg = c.orange7 },
-    ['@tag.delimiter'] = { fg = c.orange7 },
     ['@tag.attribute'] = { fg = c.yellow5 },
+    ['@tag.delimiter'] = { fg = c.orange7 },
     ['@variable'] = { fg = c.text },
 
-    ['@cssid'] = { fg = c.blue4 },
+    ['@lsp.type.property.lua'] = { fg = c.sea2 },
+    ['@property.lua'] = { fg = c.sea2 },
+    ['@punctuation.bracket.lua'] = { fg = c.sea3 },
+    ['@punctuation.delimiter.lua'] = { fg = c.text },
+    ['@variable.lua'] = { fg = c.sea2 },
+
     ['@cssclass'] = { fg = c.sea3 },
-    ['@property.css'] = { fg = c.rose4 },
+    ['@cssid'] = { fg = c.blue4 },
     ['@function.css'] = { fg = c.rose3 },
+    ['@property.css'] = { fg = c.rose4 },
+    ['@punctuation.delimiter.css'] = { fg = c.text },
+    ['@string.css'] = { fg = c.orange3 },
     ['@tag.css'] = { fg = c.orange5 },
 
-    ['@type.svelte'] = { fg = c.blue4 },
-    ['@keyword.svelte'] = { fg = c.blue4 },
-    ['@punctuation.bracket.svelte'] = { fg = c.blue4 },
-
-    ['@variable.builtin.typescript'] = { fg = c.rose4 },
-    ['@variable.builtin.javascript'] = { fg = c.rose4 },
-
-    ['@method.vue'] = { fg = c.sea3 },
-    ['@function.method.vue'] = { fg = c.rose4 },
-    ['@variable.member.vue'] = { fg = c.green2 },
-    ['@operator.vue'] = { fg = c.orange4 },
-    ['@variable.vue'] = { fg = c.yellow3 },
-    ['@punctuation.special.vue'] = { fg = c.yellow3 },
-    ['@attr.directive.vue'] = { fg = c.blue4 },
+    ['@property.scss'] = { fg = c.rose4 },
+    ['@punctuation.delimiter.scss'] = { fg = c.text },
 
     ['@property.javascript'] = { fg = c.text },
+    ['@punctuation.bracket.javascript'] = { fg = c.text },
+    ['@punctuation.delimiter.javascript'] = { fg = c.text },
+    ['@variable.builtin.javascript'] = { fg = c.rose4 },
     ['@variable.javascript'] = { fg = c.text },
 
-    ['@text.literal.markdown_inline'] = { fg = c.organge8 },
-    ['@punctuation.delimiter.markdown_inline'] = { fg = c.organge8 },
+    ['@punctuation.bracket.typescript'] = { fg = c.sea2 },
+    ['@punctuation.delimiter.typescript'] = { fg = c.text },
+    ['@variable.builtin.typescript'] = { fg = c.rose4 },
 
-    ['@propery.lua'] = { fg = c.grey0 },
-    ['@punctuation.delimiter.lua'] = { fg = c.text },
-    ['@punctuation.bracket.lua'] = { fg = c.sea3 },
+    ['@keyword.svelte'] = { fg = c.blue4 },
+    ['@punctuation.bracket.svelte'] = { fg = c.blue4 },
+    ['@type.svelte'] = { fg = c.blue4 },
+
+    ['@punctuation.delimiter.markdown_inline'] = { fg = c.organge8 },
+    ['@text.literal.markdown_inline'] = { fg = c.organge8 },
+
+    ['@attr.directive.vue'] = { fg = c.blue4 },
+    ['@function.method.vue'] = { fg = c.rose4 },
+    ['@method.vue'] = { fg = c.sea3 },
+    ['@operator.vue'] = { fg = c.orange4 },
+    ['@punctuation.special.vue'] = { fg = c.yellow3 },
+    ['@variable.member.vue'] = { fg = c.green2 },
+    ['@variable.vue'] = { fg = c.yellow3 },
 }
