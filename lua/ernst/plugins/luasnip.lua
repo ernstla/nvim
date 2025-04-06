23aa7cb56359b7ac74af968dbdf62462884dd39f
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2025-04-06 15:50:45 +0200

    Fix luasnip mappings

--- lua/ernst/plugins/luasnip.lua
+++ lua/ernst/plugins/luasnip.lua
@@ -8,11 +8,17 @@ return { {
         ls.setup(spec.opts)
         require('ernst/snippets')
 
-        vim.keymap.set({ 'i' }, '<Tab>', function() ls.expand() end, { silent = true })
-        vim.keymap.set({ 'i', 's' }, '<C-N>', function() ls.jump(1) end, { silent = true })
-        vim.keymap.set({ 'i', 's' }, '<C-P>', function() ls.jump(-1) end, { silent = true })
+        vim.keymap.set({ 'i' }, '<Tab>', function()
+            if ls.expand_or_jumpable() then
+                ls.expand_or_jump()
+            else
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+            end
+        end, { silent = true })
+        vim.keymap.set({ 'i', 's' }, '<C-n>', function() ls.jump(1) end, { silent = true })
+        vim.keymap.set({ 'i', 's' }, '<C-p>', function() ls.jump(-1) end, { silent = true })
 
-        vim.keymap.set({ 'i', 's' }, '<C-E>', function()
+        vim.keymap.set({ 'i', 's' }, '<C-e>', function()
             if ls.choice_active() then
                 ls.change_choice(1)
             end
