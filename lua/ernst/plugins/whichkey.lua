4186110fbf8c1e27207a67503b9d6963f8c454c1
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2025-04-07 11:43:54 +0200

    Adjust whickkey key bindings

--- lua/ernst/plugins/whichkey.lua
+++ lua/ernst/plugins/whichkey.lua
@@ -3,5 +3,9 @@ return { {
     event = "VeryLazy",
     opts = {
         icons = { mappings = false },
+        keys = {
+            scroll_down = "<c-f>",
+            scroll_up = "<c-b>",
+        },
     }
 } }
