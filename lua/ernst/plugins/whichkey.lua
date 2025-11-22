d13071849af88aeb955543089bbe9e064aad1d6d
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2025-11-22 12:11:45 +0100

    Set whichkey delay to 500ms

--- lua/ernst/plugins/whichkey.lua
+++ lua/ernst/plugins/whichkey.lua
@@ -3,6 +3,7 @@ return { {
     event = "VeryLazy",
     opts = {
         icons = { mappings = false },
+        delay = 500,
         keys = {
             scroll_down = "<c-f>",
             scroll_up = "<c-b>",
