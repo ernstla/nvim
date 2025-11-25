658d93826fb1c31a2a791107809db1fd76f58455
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2025-11-25 15:52:24 +0100

    Add :Rg* commands

--- lua/ernst/command.lua
+++ lua/ernst/command.lua
@@ -7,3 +7,7 @@ command('Tidy', '!tidy -mi -html -wrap 0 %', {})
 command('LspInfo', 'checkhealth vim.lsp', {})
 command('LspAttached', 'lua vim.print(vim.lsp.get_clients({ bufnr = 0 }))', {})
 command("ReloadColorscheme", function() require('ernst/lib').reload_colorscheme() end, {})
+
+command('Rg', function(opts) vim.cmd('silent! :botright :grep ' .. opts.args) end, { nargs = '*' })
+command('Rgi', function(opts) vim.cmd('silent! :botright :grep --ignore-case ' .. opts.args) end, { nargs = '*' })
+command('Rgs', function(opts) vim.cmd('silent! :botright :grep --case-sensitive ' .. opts.args) end, { nargs = '*' })
