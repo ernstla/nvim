cdbe83f8544e727f2a726325488b8abe5bf51ce1
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-02-25 09:56:24 +0100

    Add spectre mappings

--- lua/ernst/plugins/spectre.lua
+++ lua/ernst/plugins/spectre.lua
@@ -29,6 +29,16 @@ return { {
                     '<cmd>lua require("spectre").toggle()<CR>',
                     desc = "Toggle Spectre"
                 },
+                {
+                    '<leader>sw',
+                    '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
+                    desc = "Search current word"
+                },
+                {
+                    '<leader>sp',
+                    '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+                    desc = "Search on current file"
+                },
             } },
             { {
                 mode = { "v" },
