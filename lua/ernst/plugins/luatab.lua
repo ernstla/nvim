97d750ba81dc59b95a4c27cbf0069ed0c17e0c5b
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-02-25 09:55:36 +0100

    Better modified indicator in tabs

--- lua/ernst/plugins/luatab.lua
+++ lua/ernst/plugins/luatab.lua
@@ -1,7 +1,16 @@
 return { {
     'alvarosevilla95/luatab.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons',
+    opts = {
+        -- title = function() return '' end,
+        modified = function(bufnr)
+            return vim.fn.getbufvar(bufnr, '&modified') == 1 and '%#TabLineModified# %*' or ''
+        end,
+        -- windowCount = function() return '' end,
+        -- devicon = function() return '' end,
+        -- separator = function() return '' end,
+    },
     config = function(_, opts)
         require('luatab').setup(opts)
-    end
+    end,
 } }
