8efc61eb482c41699213bd4da3438746265d012c
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-04-02 17:21:05 +0200

    Add vim.pack helpers

---
+++ lua/ernst/pack.lua
@@ -0,0 +1,16 @@
+vim.api.nvim_create_user_command('Update', function()
+    vim.pack.update(nil, { offline = true })
+end, { desc = 'Check for plugin updates' })
+
+vim.api.nvim_create_user_command('Clean', function(opts)
+    local plugins = vim.iter(vim.pack.get(nil, { info = false })):filter(
+        function(pkg)
+            return not pkg.active
+        end):map(
+        function(pkg)
+            return pkg.spec.name
+        end):totable()
+    for _, name in ipairs(plugins) do
+        vim.pack.del({ name }, { force = opts.bang })
+    end
+end, { desc = 'Clean unconfigured plugins' })
