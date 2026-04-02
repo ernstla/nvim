a058af2c615af322205385a7214537081f2c3979
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-04-02 16:49:56 +0200

    Add builtin undotree

--- lua/ernst/plugins/undotree.lua
+++ lua/ernst/plugins/undotree.lua
@@ -1,6 +1,2 @@
-return { {
-    'mbbill/undotree',
-    config = function()
-        vim.keymap.set('n', '<f5>', vim.cmd.UndotreeToggle)
-    end
-} }
+vim.cmd('packadd nvim.undotree')
+vim.keymap.set("n", '<f5>', require("undotree").open)
