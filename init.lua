b52413ff2195a3a75980d31b9f94b9e074afaa19
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-04-02 12:46:17 +0200

    Load global dependencies in init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,20 @@
+vim.pack.add({
+    'https://github.com/nvim-lua/plenary.nvim',
+    'https://github.com/nvim-tree/nvim-web-devicons',
+    'https://github.com/folke/which-key.nvim',
+    'https://github.com/nvim-treesitter/nvim-treesitter',
+    'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
+})
+
+require('which-key').setup({
+    icons = { mappings = false },
+    delay = 500,
+    keys = {
+        scroll_down = "<c-f>",
+        scroll_up = "<c-b>",
+    },
+})
+
 vim.cmd.colorscheme('serious')
 require('ernst/settings')
 require("ernst")
--- plugin/aaa-deps.lua
+++
@@ -1,7 +0,0 @@
-vim.pack.add({
-    'https://github.com/nvim-lua/plenary.nvim',
-    'https://github.com/nvim-tree/nvim-web-devicons',
-    'https://github.com/folke/which-key.nvim',
-    'https://github.com/nvim-treesitter/nvim-treesitter',
-    'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
-})
