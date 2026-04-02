c13cc8741f3d1b00f539b34676c6be71a75cdcb6
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-04-02 12:01:49 +0200

    Move which-key, devicons, and treesitter to vim.pack

---
+++ plugin/which-key.lua
@@ -0,0 +1,10 @@
+-- NOTE: installed via aaa-deps.lua
+
+require('which-key').setup({
+    icons = { mappings = false },
+    delay = 500,
+    keys = {
+        scroll_down = "<c-f>",
+        scroll_up = "<c-b>",
+    },
+})
