9cfa3c4a231da78d7857ecaf77b89a5cf4a4b1c8
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-04-02 08:53:14 +0200

    Update conform formatters

--- lua/ernst/plugins/conform.lua
+++ lua/ernst/plugins/conform.lua
@@ -21,9 +21,15 @@ return { {
         formatters_by_ft = {
             css = { "prettierd", "prettier", stop_after_first = true },
             scss = { "prettierd", "prettier", stop_after_first = true },
-            html = { "php_cs_fixer" },
+            html = { "mago_format", "php_cs_fixer", stop_after_first = true },
             javascript = { "prettierd", "prettier", stop_after_first = true },
-            php = { "php_cs_fixer" },
+            php = function(bufnr)
+                if require("conform").get_formatter_info("mago_format", bufnr).available then
+                    return { "mago_format", "mago_lint" }
+                else
+                    return { "php_cs_fixer" }
+                end
+            end,
             python = function(bufnr)
                 if require("conform").get_formatter_info("ruff_format", bufnr).available then
                     return { "ruff_format", "ruff_fix" }
@@ -37,6 +43,7 @@ return { {
             json = { "prettierd", "prettier", stop_after_first = true },
             jsonc = { "prettierd", "prettier", stop_after_first = true },
             nim = { "nph", "nimpretty", stop_after_first = true },
+            markdown = { "prettierd", "prettier", stop_after_first = true },
         },
         format_on_save = {
             lsp_format = "fallback",
