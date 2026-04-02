61f79d62a6ba4cb69c0f353f893948580f57c576
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-04-02 14:17:19 +0200

    Move plugins config back to lua/ernst

--- init.lua
+++ init.lua
@@ -1,22 +1,4 @@
-vim.pack.add({
-    'https://github.com/nvim-lua/plenary.nvim',
-    'https://github.com/nvim-tree/nvim-web-devicons',
-    'https://github.com/folke/which-key.nvim',
-    'https://github.com/nvim-treesitter/nvim-treesitter',
-    'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
-    'https://github.com/MunifTanjim/nui.nvim',
-    'https://github.com/ibhagwan/fzf-lua',
-})
-
-require('which-key').setup({
-    icons = { mappings = false },
-    delay = 500,
-    keys = {
-        scroll_down = "<c-f>",
-        scroll_up = "<c-b>",
-    },
-})
-
+require('ernst/plugins')
 vim.cmd.colorscheme('serious')
 require('ernst/settings')
 require("ernst")
--- lua/ernst/plugins/init.lua
+++ lua/ernst/plugins/init.lua
@@ -1,10 +1,50 @@
-return {
-    'bronson/vim-visual-star-search',
-    'mattn/emmet-vim',
-    'mg979/vim-visual-multi',
-    'pangloss/vim-javascript',
+-- Dependencies required by other plugins
+vim.pack.add({
+    'https://github.com/nvim-lua/plenary.nvim',
+    'https://github.com/nvim-tree/nvim-web-devicons',
+    'https://github.com/folke/which-key.nvim',
+    'https://github.com/nvim-treesitter/nvim-treesitter',
+    'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
+    'https://github.com/MunifTanjim/nui.nvim',
+    'https://github.com/ibhagwan/fzf-lua',
+})
 
-    'tpope/vim-repeat',
-    'tpope/vim-surround',
-    'tpope/vim-fugitive',
-}
+vim.pack.add({
+    'https://github.com/bronson/vim-visual-star-search',
+    'https://github.com/mattn/emmet-vim',
+    'https://github.com/mg979/vim-visual-multi',
+    'https://github.com/pangloss/vim-javascript',
+    'https://github.com/tpope/vim-repeat',
+    'https://github.com/tpope/vim-surround',
+    'https://github.com/tpope/vim-fugitive',
+})
+
+-- Plugin configs where loading order matters
+require('which-key').setup({
+    icons = { mappings = false },
+    delay = 500,
+    keys = {
+        scroll_down = "<c-f>",
+        scroll_up = "<c-b>",
+    },
+})
+require('ernst/plugins/parrot')
+
+-- Everything else
+require('ernst/plugins/aerial')
+require('ernst/plugins/blink')
+require('ernst/plugins/codediff')
+require('ernst/plugins/comment')
+require('ernst/plugins/conform')
+require('ernst/plugins/delta')
+require('ernst/plugins/fzf')
+require('ernst/plugins/gitsigns')
+require('ernst/plugins/lualine')
+require('ernst/plugins/luasnip')
+require('ernst/plugins/mason')
+require('ernst/plugins/nvim-treesitter')
+require('ernst/plugins/nvim-web-devicons')
+require('ernst/plugins/oil')
+require('ernst/plugins/telescope')
+require('ernst/plugins/tmux')
+require('ernst/plugins/trouble')
