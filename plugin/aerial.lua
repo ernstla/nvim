8f07db23bf0d8b051d0668746757295875d3a0fa
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-04-02 12:08:07 +0200

    Move aerial.nvim to vim.pack

---
+++ plugin/aerial.lua
@@ -0,0 +1,50 @@
+vim.pack.add({ 'https://github.com/stevearc/aerial.nvim' })
+
+require('aerial').setup({
+    layout = {
+        max_width = 30,
+        width = 30,
+        min_width = 30,
+        default_direction = "prefer_left",
+        resize_to_content = false,
+    },
+    nav = {
+        border = "rounded",
+        max_height = 0.9,
+        min_height = { 41, 0.1 },
+        keymaps = {
+            ["<CR>"] = "actions.jump",
+            ["<2-LeftMouse>"] = "actions.jump",
+            ["<C-v>"] = "actions.jump_vsplit",
+            ["<C-s>"] = "actions.jump_split",
+            ["h"] = "actions.left",
+            ["l"] = "actions.right",
+            ["<esc>"] = "actions.close",
+            ["q"] = "actions.close",
+        },
+    }
+})
+
+require("which-key").add({ {
+    mode = { "n" },
+    {
+        '<leader>ss',
+        '<cmd>AerialNavOpen<cr>',
+        desc = "aerial: open "
+    },
+    {
+        '<leader>st',
+        '<cmd>Telescope aerial<cr>',
+        desc = "aerial: telescope"
+    },
+    {
+        '<leader>sf',
+        '<cmd>AerialToggle float<cr>',
+        desc = "aerial: float"
+    },
+    {
+        '<leader>sa',
+        '<cmd>AerialToggle<cr>',
+        desc = "aerial: toggle"
+    },
+} })
