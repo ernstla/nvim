1688d17d119dd1e5b52eb59bf8227f34c32136c4
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-04-09 11:49:26 +0200

    Remove util.project_files

--- lua/util/init.lua
+++ lua/util/init.lua
@@ -11,16 +11,4 @@ function M.reload_colorscheme()
     vim.cmd.colorscheme('serious')
 end
 
-M.project_files = function()
-    local opts = {} -- define here if you want to define something
-
-    vim.fn.system('git rev-parse --is-inside-work-tree')
-
-    if vim.v.shell_error == 0 then
-        require "telescope.builtin".git_files(opts)
-    else
-        require "telescope.builtin".find_files(opts)
-    end
-end
-
 return M
