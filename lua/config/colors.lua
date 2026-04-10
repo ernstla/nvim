61bd6c6092b52643f1427724b8ecd9c7d4cd429e
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-04-10 19:34:21 +0200

    Make :Colors toggle per-buffer instead of global

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -2,5 +2,8 @@ local command = vim.api.nvim_create_user_command
 
 vim.lsp.document_color.enable(false)
 
-command('Colors', function() vim.lsp.document_color.enable(not vim.lsp.document_color.is_enabled()) end, {})
+command('Colors', function()
+    local filter = { bufnr = 0 }
+    vim.lsp.document_color.enable(not vim.lsp.document_color.is_enabled(filter), filter)
+end, {})
 command('ColorsConvert', function() vim.lsp.document_color.color_presentation() end, {})
