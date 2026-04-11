37c95c1e7a4943569a00cb2d8ef455118d5e9490
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-04-11 11:41:26 +0200

    Remove prettierd from conform config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -42,10 +42,10 @@ require('conform').setup({
         },
     },
     formatters_by_ft = {
-        css = { "prettierd", "prettier", stop_after_first = true },
-        scss = { "prettierd", "prettier", stop_after_first = true },
+        css = { "prettier" },
+        scss = { "prettier" },
         html = { "mago_format", "php_cs_fixer", stop_after_first = true },
-        javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { "prettier" },
         php = function(bufnr)
             if require("conform").get_formatter_info("mago_format", bufnr).available then
                 return { "mago_format", "mago_lint" }
@@ -60,13 +60,13 @@ require('conform').setup({
                 return { "isort", "black" }
             end
         end,
-        svelte = { "prettierd", "prettier", stop_after_first = true },
-        typescript = { "prettierd", "prettier", stop_after_first = true },
-        vue = { "prettierd", "prettier", stop_after_first = true },
-        json = { "prettierd", "prettier", stop_after_first = true },
-        jsonc = { "prettierd", "prettier", stop_after_first = true },
+        svelte = { "prettier" },
+        typescript = { "prettier" },
+        vue = { "prettier" },
+        json = { "prettier" },
+        jsonc = { "prettier" },
         nim = { "nph", "nimpretty", stop_after_first = true },
-        markdown = { "prettierd", "prettier", stop_after_first = true },
+        markdown = { "prettier" },
     },
     format_on_save = {
         lsp_format = "fallback",
